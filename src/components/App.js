ab4db35fbe85bf646fea624879ab7924c09897c2
Raymond Gregory <vinedsel@gmail.com>
2019-01-23 19:07:24 -0600

    Added code to components/App file.

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import Search from './Search';
 import { connect } from 'react-redux';
-import { } from '../actions/index';
+import { searchTextChanged, repoSearchCompleted, searchGithub } from '../actions/index';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h3>Search Repos:</h3>
-        <Search />
+        <Search repos={this.props.repos} onSearch={this.props.onSearch} onSearchTextChanged={this.props.onSearchTextChanged} searchText={this.props.searchText}/>
       </div>
     );
   }
 }
 
-function mapDispatchToProps(dispatch) {
-
+function mapDispatchToProps(dispatch, state) {
+  return {
+    onSearchTextChanged: (text) => dispatch(searchTextChanged(text)),
+    onSearch: (text) => searchGithub(text).then(repos => dispatch(repoSearchCompleted(repos)))
+  };
 }
 
 function mapStateToProps(state) {
-
+  return {
+      repos: state.repos,
+      searchText: state.searchInputted
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
